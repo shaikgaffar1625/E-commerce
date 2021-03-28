d9835e84f1806e13cc8f7c90597ee2d105606419
unknown <shaikgaffar1625@gmail.com>
2021-03-28 16:50:10 +0300

    added 4feb-2021

---
+++ 4feb-2021/index.js
@@ -0,0 +1,35 @@
+//number
+var a=7;
+console.log(a);
+console.log(typeof a);
+
+//boolean
+var isDone=false;
+console.log(isDone);
+ 
+console.log(typeof isDone);
+
+//string
+var str="hello!"
+console.log(str);
+console.log(typeof str);
+
+//array
+var arr=[10,20,30,40];
+console.log(arr[0]);
+(arr.push(70))
+console.log(arr)
+
+
+//object
+
+var obj={
+    name :'sulthan',
+    age :"23",
+    address :"hyd",
+    pincode :"500013",
+    salary :"90000.0",
+    technology :["java","JS"],
+
+console.log(obj.name)
+}
