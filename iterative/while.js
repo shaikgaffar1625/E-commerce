a258897c90e8a93402c5bc425fda71d5f82e51ca
unknown <shaikgaffar1625@gmail.com>
2021-04-05 16:56:20 +0300

    added iterative

---
+++ iterative/while.js
@@ -0,0 +1,34 @@
+/*var i=1;
+while (i<=20){
+    console.log(i);
+    i++;
+} */
+
+/*var users= "ravi,rajesh,ramesh";
+var index=0;
+while (index<users.length){
+    console.log(users);
+    index++;
+}*/
+
+var users=[
+    {
+        name : "ravi",
+        age  : 21,
+    },
+    {
+        name : "rajesh",
+        age  : 23,
+    },
+    {
+        name : "rakesh",
+        age  : 25,
+    },
+]
+  var index=0;
+  while (index<users.length);
+  {
+    console.log(users[index].name + "\t" + users[index].age);
+    index++;
+  }
+    
