507b82e4b1f9ee1cc7596f9bfb3f3f08884f5398
unknown <shaikgaffar1625@gmail.com>
2021-04-05 17:00:13 +0300

    added iterative

---
+++ iterative/forloop.js
@@ -0,0 +1,14 @@
+var users=[
+    {
+        name:"ravi"
+    },
+    {
+        name:"rajesh"
+    },
+    {
+        name:"rakesh"
+    },
+]
+  for(var i=0; i<users.length; i++){
+      console.log(users[i].name);
+  }
