63fad60c35e53596ed5bd0cf126797680d9f4ccb
unknown <shaikgaffar1625@gmail.com>
2021-05-06 00:24:15 +0300

    added 22feb2021

---
+++ 22feb2021/index.js
@@ -0,0 +1,28 @@
+/*call by value
+var a=10,b=20;
+function f1(x,y){
+      x=90;
+      y=100;
+  console.log(x);  
+  console.log(y);      
+}
+f1(a,b);
+console.log(a); */
+
+/*call by reference
+var arr=[1,2,3,4];
+function f1(x){
+    x.push(5);
+}
+f1(arr);
+console.log(arr); */
+
+
+/*function Employee(name,address,eid){
+    this.name;
+    this.address;
+    this.eid;
+}
+    var e1=new Employee("ravi","hyd",123);
+    console.log(e1);*/
+
