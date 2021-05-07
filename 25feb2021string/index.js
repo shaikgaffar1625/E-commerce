37b7d7e76ac3b2d7a2f4eac14445383a7a7b6128
unknown <shaikgaffar1625@gmail.com>
2021-05-08 00:39:11 +0300

    added 25feb2021string

---
+++ 25feb2021string/index.js
@@ -0,0 +1,26 @@
+/* var username="Ravikumar";
+console.log(username[0]);
+console.log(username[3]);
+console.log(username[7]); */
+
+/* var username="Ravi";
+var password="Kumar";
+console.log(username[3]);
+console.log(password[1]);   */
+
+/* var firstname="Ravi";
+var lastname="Kumar";
+firstname=firstname.concat(lastname);
+console.log(firstname); */ 
+
+
+/*var firstname="Ravi";
+var lastname="Kumar";
+console.log(firstname.toUpperCase());
+console.log(firstname.link("https://www.facebook.com"));
+
+var firstname="Ravi";
+var lastname="Kumar";
+console.log(firstname.toLocaleLowerCase());
+console.log(firstname.link("https://www.facebook.com")); */
+
