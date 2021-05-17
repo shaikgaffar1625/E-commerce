7dbd3da09d47769e8ca0f343f915054fdae81780
unknown <shaikgaffar1625@gmail.com>
2021-05-17 05:34:38 +0300

    added 8march2021

---
+++ 8march2021/index.js
@@ -0,0 +1,63 @@
+/*var a=10;
+
+var b=20;
+
+var c=90;
+
+try{
+    console.log(c);
+}
+
+catch(error){
+    console.log(error.message);
+}
+console.log(a+b); // 30           */
+
+/*var a=10;
+
+var b=20;
+
+var c=900,d,m;
+
+try{
+    console.log("db opened")
+    console.log(c);
+    console.log(d);
+    console.log(e);
+}
+catch(error){
+    console.log(error.message);
+    console.log(m);
+}
+finally{
+     console.log("db closed");
+}
+console.log(a+b); // 30      */
+
+
+
+/*var isUserLoggedIn=false;
+
+try{
+
+if(isUserLoggedIn){
+      console.log("user logged in");
+   }
+   else
+   {
+       throw new Error('unauthorized access');
+   }
+}
+catch(error){
+    console.log(error.message);
+}
+ console.log("after error")   */
+
+
+ var employee={
+    name:"ravikumar",
+    address:"hyderabad"
+}
+console.log(employee);
+
+employee=null; 
